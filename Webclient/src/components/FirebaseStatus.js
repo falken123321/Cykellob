8dd6a495b07a4f35f4b1455e9b9d48a27720c010
Neaxic <andreas79001@gmail.com>
2020-04-16 11:11:20 +0200

    Mapping Omgange virker

--- Webclient/src/components/FirebaseStatus.js
+++ Webclient/src/components/FirebaseStatus.js
@@ -6,47 +6,107 @@ import firebase from "../lib/Firebase"
 const firestore = firebase.firestore();
 const docRef = firestore.doc("Cykelløb/001");
 
+
 function FirebaseDataFunction() {
-    const thingsToDoWithDocumentData = doc => {
-        if (doc && doc.exists) {
-            const myData = doc.data();
-            setFirebaseData(myData.firstName);
-        }
-    };
+  const thingsToDoWithDocumentData = doc => {
+    if (doc && doc.exists) {
+      const myData = doc.data();
+      console.log(myData);
+      setFirebaseData(myData);
+    }
+  };
 
-    const getRealtimeUpdates = () => {
-        docRef.onSnapshot(thingsToDoWithDocumentData);
-    };
+  const getRealtimeUpdates = () => {
+    docRef.onSnapshot(thingsToDoWithDocumentData);
+  };
 
-    const [FirebaseData, setFirebaseData] = useState("");
 
-    useEffect(() => {
-        getRealtimeUpdates();
-    }); 
+  // InitialValue er brugt her fordi at React loader objektet før der er noget deri, og så crasher programmet.*
+  // På denne måde laver vi værdierne som tomme på forhånd, for at undgå crash.
 
-    return (
-        <div>
-        <h2>
-          {FirebaseData} 
-        </h2>
-        <p>Username:</p>
-        <table>
-          <thead>
-            <tr>
-              <th>Time</th>
-              <th>Score</th>
-            </tr>
-          </thead>
+  const initialValue = {
+    Name: "Loading...",
+    Klasse: "Loading...",
+    Nummer: "Loading...",
+    Omgange: [],
+    Test: []}
+  const [firebaseData, setFirebaseData] = useState(initialValue);
+
+  useEffect(() => {
+    getRealtimeUpdates();
+  });
+
+
+  const array1 = [1, 4, 9, 16];
+  // const docRef = firestore.doc.map(item) => (
   
-          <tbody>
-              <tr>
-                <td>2398388</td>
-                <td>22</td>
-              </tr>
-          </tbody>
-        </table>
+  return (
+    <div>
+            <h1>Liste over alle omgange sorteret</h1>
+      <div>
+        <label>Soter: </label>
+        <select>
+          <option>Omgange (Flest først)</option>
+          <option>Omgange (Færrest først)</option>
+        </select>
       </div>
-    );
+      <table>
+        <thead>
+          <tr>
+            <th>Navn</th>
+            <th>Omgange</th>
+          </tr>
+        </thead>
+
+        <tbody>
+
+          <tr>
+
+          </tr>
+
+
+          <tr>
+            <td>Andreas Østergaard</td>
+            <td>22</td>
+          </tr>
+          <tr>
+            <td>Lalsl Østergaard</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+      <h1>Resultater fra alle deltagere induvidielt</h1>
+      <p>Cykkelrytter: {firebaseData.Name} </p>
+      <p>Klassetrin: {firebaseData.Klasse}</p>
+      <p>Cykkelnummer: {firebaseData.Nummer}</p>
+      <p>Cykkelnummer: {firebaseData.Test}</p>
+      
+      <h3>Antal omgange cyklet</h3>
+
+      <table>
+        <thead>
+          <tr>
+          <th>Tid</th>
+            <th>Omgangs Nummer</th>
+            
+          </tr>
+        </thead>
+
+        <tbody>
+          {
+            firebaseData.Omgange.map(
+              (item, i) => (
+                <tr>
+                  <td> {item.toDate().toISOString()}</td>
+                  <td>{i}</td>
+                </tr>
+              )
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default FirebaseDataFunction;
